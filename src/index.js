f196dba8aadc0af74e0bd51721381997b951b203
dubeyshiv123 <dubeyshiv0123@gmail.com>
2023-07-27 19:19:39 +0530

    Added create ticket API

--- src/index.js
+++ src/index.js
@@ -13,15 +13,4 @@ app.use("/api", apiRoutes);
 
 app.listen(ServerConfig.PORT, async () => {
   console.log(`Successfully started the server on PORT : ${ServerConfig.PORT}`);
-  try {
-    const response = await mailsender.sendMail({
-      from: ServerConfig.GMAIL_EMAIL,
-      to: "notific.serv@gmail.com",
-      subject: "Is the service working ?",
-      text: "Yes it is working",
-    });
-    console.log(response);
-  } catch (error) {
-    console.log(error);
-  }
 });
